887994eef950d93935aa09bf2643461677514b1a
Robbyro89 <gallery.laroche.id@gmail.com>
2018-02-02 13:58:31 +0800

    Creation du model Moussaillon

---
+++ db/migrate/20180202055741_create_moussaillons.rb
@@ -0,0 +1,11 @@
+class CreateMoussaillons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :moussaillons do |t|
+      t.string :pseudo
+      t.string :email
+      t.text :biographie
+
+      t.timestamps
+    end
+  end
+end

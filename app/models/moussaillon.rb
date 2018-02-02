cbb56168f0f84a197129630d9ca31581c512c835
Robbyro89 <gallery.laroche.id@gmail.com>
2018-02-03 02:00:48 +0800

    Edit page home

--- app/models/moussaillon.rb
+++ app/models/moussaillon.rb
@@ -1,2 +1,8 @@
 class Moussaillon < ApplicationRecord
+  validates :pseudo, presence: true, length: { maximum: 50 },
+             uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 },
+             VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+             format: { with: VALID_EMAIL_REGEX }
+  validates :biographie, presence: true, length: { minimum: 5 }
 end
